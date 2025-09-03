a3d03c69022bf58200fe5fa4c9e0a9abc68d636d
LeoDuffed <carlosmroder@gmail.com>
2025-09-03 15:32:52 -0600

    Codigo comentado de la ruta messages

--- ayolin-mini/src/app/api/chats/[chatId]/messages/route.ts
+++ ayolin-mini/src/app/api/chats/[chatId]/messages/route.ts
@@ -3,14 +3,14 @@ import { db } from "@/lib/db";
 
 // Ruta para poder obtener mensajes de un chat
 export async function GET(_req: NextRequest, { params }: { params: Promise<{chatId: string }> }){
-    const { chatId } = await params
-    const chat = await db.chat.findUnique({ where: { id:chatId } } )
-    if(!chat) return NextResponse.json({error: 'Chat not found' }, { status: 404})
+    const { chatId } = await params // recibimos el chatId, esperamos un objeto { chatId: "valor" }
+    const chat = await db.chat.findUnique({ where: { id:chatId } } ) // Vemos si existe un chat en la db
+    if(!chat) return NextResponse.json({error: 'Chat not found' }, { status: 404}) // Si no mandamos un Not Found
     
     const messages = await db.message.findMany({
-        where: { chatId },
-        orderBy: { createdAt: 'asc' },
-        select: { id: true, role: true, content: true, createdAt: true },
-    })
-    return NextResponse.json({ messages })
+        where: { chatId }, // Buscamos los mns del chat
+        orderBy: { createdAt: 'asc' }, // Los ordenamos del mas antiguo al ultimo
+        select: { id: true, role: true, content: true, createdAt: true }, // Campos importantes
+    }) 
+    return NextResponse.json({ messages }) // Devolvemos los mns 
 }
