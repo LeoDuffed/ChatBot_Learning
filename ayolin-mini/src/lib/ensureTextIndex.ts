a9add72039b7b0a4c4f147d49653875d277b52fb
LeoDuffed <carlosmroder@gmail.com>
2025-09-29 13:47:12 -0600

    cambio necesario

--- ayolin-mini/src/lib/ensureTextIndex.ts
+++ ayolin-mini/src/lib/ensureTextIndex.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { PrismaClient } from "@/generated/prisma"
+import { PrismaClient } from "@prisma/client"
 
 type CreateIndexesCommand = {
   createIndexes: string,
